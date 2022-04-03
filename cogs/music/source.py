22f7260b0218d67b05ab4f8422dfbfc0414c36f1
freezpmark <p.markus1337@gmail.com>
2022-04-04 01:11:08 +0900

    try

--- cogs/music/source.py
+++ cogs/music/source.py
@@ -82,4 +82,4 @@ class YTDLSource(discord.PCMVolumeTransformer):
         to_run = functools.partial(ytdl.extract_info, url=data['webpage_url'], download=False)
         data = await loop.run_in_executor(None, to_run)
 
-        return cls(discord.FFmpegPCMAudio(data['url'], **ffmpeg_opts), data=data, requester=requester)
+        return cls(discord.FFmpegPCMAudio(data['url']), data=data, requester=requester)
