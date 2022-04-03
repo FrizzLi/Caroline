908d3fd55ed1b8205ad198874a4e9c6c0446506d
freezpmark <p.markus1337@gmail.com>
2022-04-03 19:38:09 +0900

    [UPD] commands, reorganize, clean and check more

--- cogs/music/music.py
+++ cogs/music/music.py
@@ -52,6 +52,74 @@ class Music(commands.Cog):
         if voice_state is not None and len(voice_state.channel.members) == 1:
             await self.cleanup(member.guild)
 
+    # Slash commands, the main command
+    @app_commands.command(name='play')
+    async def play_(self, interaction, *, search: str):
+        """Request a song and add it to the queue.
+        This command attempts to join a valid voice channel if the bot is not already in one.
+        Uses YTDL to automatically search, retrieves a song and streams it.
+
+        Args:
+            search: str [Required]
+                The song to search and retrieve using YTDL. This could be a simple search, an ID or URL.
+        """
+
+        # making sure interaction timeout does not expire
+        await interaction.response.send_message("...Looking for song(s)... wait...")
+
+        # check if we're in channel, if not, join the one we are currently in
+        vc = interaction.guild.voice_client
+        if not vc:
+            channel = interaction.user.voice.channel
+            await channel.connect()  # simplified cuz cannot invoke connect_
+
+        # If download is False, source will be a list of entries which will be used to regather the stream.
+        # If download is True, source will be a discord.FFmpegPCMAudio with a VolumeTransformer.
+        try:
+            entries = await YTDLSource.create_source(interaction, search, loop=self.bot.loop, download=False)
+        except DownloadError as e:
+            await interaction.followup.send(e)
+            return
+
+        player = self.get_player(interaction)
+        for entry in entries:
+            source = {
+                'webpage_url': entry['webpage_url'],
+                'requester': interaction.user.display_name,
+                'title': entry['title'],
+            }
+            player.queue.append(source)
+
+        await player.dummy_queue.put(True)
+
+    @app_commands.command(name='volume')
+    async def change_volume(self, interaction, *, volume: int=None):
+        """Change or see the volume of player in percentages.
+
+        Args:
+            volume: int
+                The volume to set the player to in percentage. This must be between 1 and 100.
+        """
+
+        player = self.get_player(interaction)
+        if volume is None:
+            return await interaction.response.send_message(f"The volume is currently at **{int(player.volume*100)}%**.")
+        elif not 0 < volume < 101:
+            return await interaction.response.send_message("Please enter a value between 1 and 100.")
+
+        vc = interaction.guild.voice_client
+        if vc and vc.is_connected() and vc.source:
+            vc.source.volume = volume / 100
+
+        old_volume = player.volume * 100
+        player.volume = volume / 100
+
+        embed = discord.Embed(
+            description=f'The volume has been set from **{int(old_volume)}%** to **{volume}%**',
+            color=discord.Color.green()
+        )
+        await interaction.response.send_message(embed=embed)
+
     @app_commands.command(name='join')
     async def connect_(self, interaction, *, channel: discord.VoiceChannel=None):
         """Connect to voice.
@@ -93,50 +161,34 @@ class Music(commands.Cog):
             except asyncio.TimeoutError:
                 await interaction.response.send_message(f'Connecting to channel: <{channel}> timed out.')
 
-    @app_commands.command(name='play')
-    async def play_(self, interaction, *, search: str):
-        """Request a song and add it to the queue.
-        This command attempts to join a valid voice channel if the bot is not already in one.
-        Uses YTDL to automatically search, retrieves a song and streams it.
+    @app_commands.command(name='leave')
+    async def leave_(self, interaction):
+        """Stop the currently playing song, clears queue and disconnects from voice."""
 
-        Args:
-            search: str [Required]
-                The song to search and retrieve using YTDL. This could be a simple search, an ID or URL.
-        """
-
-        # making sure interaction timeout does not expire
-        await interaction.response.send_message("...Looking for song(s)... wait...")
-
-        # check if we're in channel, if not, join the one we are currently in
         vc = interaction.guild.voice_client
-        if not vc:
-            channel = interaction.user.voice.channel
-            await channel.connect()  # simplified cuz cannot invoke connect_
+        if not vc or not vc.is_connected():
+            return await interaction.response.send_message("I'm not connected to a voice channel.")
 
-        # If download is False, source will be a list of entries which will be used to regather the stream.
-        # If download is True, source will be a discord.FFmpegPCMAudio with a VolumeTransformer.
-        try:
-            entries = await YTDLSource.create_source(interaction, search, loop=self.bot.loop, download=False)
-        except DownloadError as e:
-            await interaction.followup.send(e)
-            return
+        await self.cleanup(interaction.guild)
 
-        player = self.get_player(interaction)
-        for entry in entries:
-            source = {
-                'webpage_url': entry['webpage_url'],
-                'requester': interaction.user.display_name,
-                'title': entry['title'],
-            }
-            player.queue.append(source)
+        embed = discord.Embed(
+            description=f'Left **{vc.channel.name}** channel.',
+            color=discord.Color.green()
+        )
+        await interaction.response.send_message(embed=embed)
 
-        await player.dummy_queue.put(True)
-
+    # Invoked commands with voice check
     @app_commands.command(name="jump")
     async def jump_(self, interaction, index: int):
         """Jumps to specific track after currently playing song finishes."""
 
+        vc = interaction.guild.voice_client
+        if not vc or not vc.is_connected():
+            return await interaction.response.send_message("I'm not connected to a voice channel.")
+
         player = self.get_player(interaction)
+        if not player.queue:
+            return await interaction.response.send_message("There is no queue.")
         if 0 >= index or index > len(player.queue):
             return await interaction.response.send_message(f"Could not find a track at '{index}' index.")
 
@@ -159,10 +211,9 @@ class Music(commands.Cog):
         player = self.get_player(interaction)
         if not player.queue:
             return await interaction.response.send_message("There is no queue.")
-
         if index is None:
             index = len(player.queue)
-        elif index > len(player.queue):
+        elif 0 >= index > len(player.queue):
             return await interaction.response.send_message(f"Could not find a track at '{index}' index.")
 
         s = player.queue[index-1]
@@ -198,55 +249,18 @@ class Music(commands.Cog):
         )
         await interaction.response.send_message(embed=embed)
 
-    @app_commands.command(name='volume')
-    async def change_volume(self, interaction, *, volume: int=None):
-        """Change or see the volume of player in percentages.
-
-        Args:
-            volume: int
-                The volume to set the player to in percentage. This must be between 1 and 100.
-        """
-
-        player = self.get_player(interaction)
-        if volume is None:
-            return await interaction.response.send_message(f"The volume is currently at **{int(player.volume*100)}%**.")
-        elif not 0 < volume < 101:
-            return await interaction.response.send_message("Please enter a value between 1 and 100.")
-
-        vc = interaction.guild.voice_client
-        if vc and vc.is_connected() and vc.source:
-            vc.source.volume = volume / 100
-
-        old_volume = player.volume * 100
-        player.volume = volume / 100
-
-        embed = discord.Embed(
-            description=f'The volume has been set from **{int(old_volume)}%** to **{volume}%**',
-            color=discord.Color.green()
-        )
-        await interaction.response.send_message(embed=embed)
-
-    @app_commands.command(name='leave')
-    async def leave_(self, interaction):
-        """Stop the currently playing song, clears queue and disconnects from voice."""
+    # Button commands
+    async def pause_(self, interaction):
+        """Pause the currently playing song."""
 
         vc = interaction.guild.voice_client
         if not vc or not vc.is_connected():
             return await interaction.response.send_message("I'm not connected to a voice channel.")
 
-        await self.cleanup(interaction.guild)
-
-        embed = discord.Embed(
-            description=f'Left **{vc.channel.name}** channel.',
-            color=discord.Color.green()
-        )
-        await interaction.response.send_message(embed=embed)
-
-    async def pause_(self, interaction):
-        """Pause the currently playing song."""
-
-        vc = interaction.guild.voice_client
-        if not vc or not vc.is_playing():
+        player = self.get_player(interaction)
+        if not player.queue:
+            return await interaction.response.send_message("There is no queue.")
+        if not vc.is_playing():
             return await interaction.response.send_message("I'm not currently playing anything.")
         elif vc.is_paused():
             return await interaction.response.send_message("The track is already paused.")
@@ -259,8 +273,14 @@ class Music(commands.Cog):
         vc = interaction.guild.voice_client
         if not vc or not vc.is_connected():
             return await interaction.response.send_message("I'm not connected to a voice channel.")
+
+        player = self.get_player(interaction)
+        if not player.queue:
+            return await interaction.response.send_message("There is no queue.")
+        if not vc.is_playing():
+            return await interaction.response.send_message("I'm not currently playing anything.")
         elif not vc.is_paused():
-            return await interaction.response.send_message("The track is being played already.")
+            return await interaction.response.send_message("The track is already being played.")
 
         vc.resume()
 
@@ -271,12 +291,30 @@ class Music(commands.Cog):
         if not vc or not vc.is_connected():
             return await interaction.response.send_message("I'm not connected to a voice channel.")
 
-        if vc.is_paused():
-            pass
-        elif not vc.is_playing():
-            return
+        player = self.get_player(interaction)
+        if not player.queue:
+            return await interaction.response.send_message("There is no queue.")
+        if not vc.is_playing():
+            return await interaction.response.send_message("I'm not currently playing anything.")
 
         vc.stop()
+
+    async def shuffle_(self, interaction):
+        """Randomizes the position of tracks in queue."""
+
+        vc = interaction.guild.voice_client
+        if not vc or not vc.is_connected():
+            return await interaction.response.send_message("I'm not connected to a voice channel.")
+
+        player = self.get_player(interaction)
+        if not player.queue:
+            return await interaction.response.send_message("There is no queue.")
+
+        shuffled_remains = player.queue[player.current_pointer+1:]
+        random.shuffle(shuffled_remains)
+
+        player.queue = player.queue[:player.current_pointer+1] + shuffled_remains
+
 
     async def loop_queue_(self, interaction):
         """Loops the queue of tracks."""
@@ -290,20 +328,9 @@ class Music(commands.Cog):
         player = self.get_player(interaction)
         player.loop_track = not player.loop_track
 
-    async def shuffle_(self, interaction):
-        """Randomizes the position of tracks in queue."""
-
-        player = self.get_player(interaction)
-
-        shuffled_remains = player.queue[player.current_pointer+1:]
-        random.shuffle(shuffled_remains)
-
-        player.queue = player.queue[:player.current_pointer+1] + shuffled_remains
-
-
 async def setup(bot):
     await bot.add_cog(
-        Music(bot), 
+        Music(bot),
         guilds=[discord.Object(
             id=os.environ.get("SERVER_ID")
         )]
