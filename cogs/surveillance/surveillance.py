4534d012b44d4f70da62df371b6fc733163b4623
freezpmark <p.markus1337@gmail.com>
2022-05-19 14:47:50 +0200

    [UPD] optimize, polish code [REM] config as its not needed to track text channels

--- cogs/surveillance/surveillance.py
+++ cogs/surveillance/surveillance.py
@@ -10,15 +10,10 @@ from discord.utils import get
 
 class Surveillance(commands.Cog):
     def __init__(self, bot):
-        surv_dir = os.path.dirname(os.path.abspath(__file__))
-        with open(f"{surv_dir}\\surv_config.json", "r") as cf:
-            config = json.load(cf)
-
         self.bot = bot
-        self.config = config
 
     def get_log_channel(self):
-        return self.bot.get_channel(self.config["IDs"]["logs"])
+        return self.bot.get_channel(556514691069902870)
 
     def get_time(self):
         time = datetime.now(pytz.timezone("Europe/Bratislava"))
@@ -27,122 +22,83 @@ class Surveillance(commands.Cog):
 
     @commands.Cog.listener()
     async def on_presence_update(self, before, after):
-
-        off     = (before.status != discord.Status.offline) and after.status == discord.Status.offline
-        no_off  = (before.status == discord.Status.offline) and after.status != discord.Status.offline
-        idle    = (before.status != discord.Status.idle) and after.status == discord.Status.idle
-        no_idle = (before.status == discord.Status.idle) and after.status != discord.Status.idle
-        dnd     = (before.status != discord.Status.dnd) and after.status == discord.Status.dnd
-        no_dnd  = (before.status == discord.Status.dnd) and after.status != discord.Status.dnd
+        off     = before.status != discord.Status.offline and after.status == discord.Status.offline
+        no_off  = before.status == discord.Status.offline and after.status != discord.Status.offline
+        idle    = before.status != discord.Status.idle and after.status == discord.Status.idle
+        no_idle = before.status == discord.Status.idle and after.status != discord.Status.idle
+        dnd     = before.status != discord.Status.dnd and after.status == discord.Status.dnd
+        no_dnd  = before.status == discord.Status.dnd and after.status != discord.Status.dnd
         act     = before.activity != after.activity and before.activity is None
         no_act  = before.activity != after.activity and after.activity is None
 
-        msgs = []
-        if off:
-            msgs.append("has come online.")
-        if no_idle:
-            msgs.append("is no longer idle.")
-        if dnd:
-            msgs.append("has set DND status.")
-        if no_dnd:
-            msgs.append("is no longer DND.")
-        if idle:
-            msgs.append("has gone idle.")
-        elif no_act:
-            msgs.append(f"has stopped {before.activity.name}.")
-        elif act:
-            msgs.append(f"has started {after.activity.name}.")
         if no_off:
-            msgs.append("has gone offline.")
+            msg = "has come online."
+        elif off:
+            msg = "has gone offline."
+        elif no_act:
+            msg = f"has stopped {before.activity.name}."
+        elif act:
+            msg = f"has started {after.activity.name}."
+        elif dnd:
+            msg = "has set DND status."
+        elif no_dnd:
+            msg = "is no longer DND."
+        elif no_idle:
+            msg = "is no longer idle."
+        elif idle:
+            msg = "has gone idle."
 
         time = self.get_time()
-        for msg in msgs:
-            await self.get_log_channel().send(f"{time}: {after.name} {msg}")
+        await self.get_log_channel().send(f"{time}: {after.name} {msg}")
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-
-        deaf    = ((before.deaf is False) and after.deaf) or ((before.self_deaf is False) and after.self_deaf)
-        no_deaf = ((before.deaf is True) and not after.deaf) or ((before.self_deaf is True) and not after.self_deaf)
-        mute    = ((before.mute is False) and after.mute) or ((before.self_mute is False) and after.self_mute)
-        no_mute = ((before.mute is True) and not after.mute) or ((before.self_mute is True) and not after.self_mute)
-        bcast   = (before.self_video is False) and after.self_video
-        no_bcast = (before.self_video is True) and not after.self_video
-        afk     = (before.afk is False) and after.afk
-        no_afk  = (before.afk is True) and not after.afk
+        deaf    = (not before.deaf and after.deaf) or (not before.self_deaf and after.self_deaf)
+        no_deaf = (before.deaf and not after.deaf) or (before.self_deaf and not after.self_deaf)
+        mute    = (not before.mute and after.mute) or (not before.self_mute and after.self_mute)
+        no_mute = (before.mute and not after.mute) or (before.self_mute and not after.self_mute)
+        video   = not before.self_video and after.self_video
+        no_video = before.self_video and not after.self_video
+        stream  = not before.self_stream and after.self_stream
+        no_stream = before.self_stream and not after.self_stream
+        afk     = not before.afk and after.afk
+        no_afk  = before.afk and not after.afk
         left    = before.channel != after.channel and after.channel is None
         join    = before.channel != after.channel and before.channel is None
         move    = before.channel != after.channel
 
-        msgs = []
         if deaf:
-            msgs.append("has been deafened.")
+            msg = "has been deafened."
         elif no_deaf:
-            msgs.append("has been undeafened.")
+            msg = "has been undeafened."
         elif mute:
-            msgs.append("has been muted.")
+            msg = "has been muted."
         elif no_mute:
-            msgs.append("has been unmuted.")
-        elif bcast:
-            msgs.append("has started broadcasting a video.")
-        elif no_bcast:
-            msgs.append("has stopped broadcasting a video.")
+            msg = "has been unmuted."
+        elif video:
+            msg = "has enabled camera."
+        elif no_video:
+            msg = "has stopped camera."
+        elif stream:
+            msg = "has started streaming."
+        elif no_stream:
+            msg = "has stopped streaming."
         elif afk:
-            msgs.append("has gone afk.")
+            msg = "has gone afk."
         elif no_afk:
-            msgs.append("is no longer afk.")
+            msg = "is no longer afk."
         elif left:
-            msgs.append(f"has left {before.channel} channel.")
+            msg = f"has left {before.channel} channel."
         elif join:
-            msgs.append(f"has joined {after.channel} channel.")
+            msg = f"has joined {after.channel} channel."
         elif move:
-            msgs.append(f"has moved from {before.channel} channel into {after.channel}.")
+            msg = f"has moved from {before.channel} channel into {after.channel}."
 
         time = self.get_time()
-        for msg in msgs:
-            await self.get_log_channel().send(f"{time}: {after.name} {msg}")
-
-    # TODO: Config with Gsheets
-    @commands.is_owner()
-    @commands.command(brief="Bypass surveillance settings.++")
-    async def bypass(self, ctx, channel=None, val=None):
-        """ "?bypass <channel_name> 1" - adds channel for bypassing
-        "?bypass <channel_name> 0" - removes channel from bypassing"""
-
-        if channel and val:
-            val = "1" if val != "0" else "0"
-            if val == "1" and channel in self.config["bypass_channels"]:
-                return await ctx.send(
-                    f'"{channel}" channel is already being bypassed!'
-                )
-            elif val == "0" and channel not in self.config["bypass_channels"]:
-                return await ctx.send(
-                    f'"{channel}" channel isnt bypassed!'
-                )
-
-            if val != "0":
-                self.config["bypass_channels"].append(channel)
-                msg = f"{channel} channel is now being untracked."
-            else:
-                self.config["bypass_channels"].remove(channel)
-                msg = f"{channel} channel is now being tracked."
-
-            surv_dir = os.path.dirname(os.path.abspath(__file__))
-            with open(surv_dir + "\\surv_config.json", "w") as f4:
-                json.dump(self.config, f4, sort_keys=True, indent=4)
-
-            await self.get_log_channel().send(msg)
-            return await ctx.send(msg)
-
-        else:  # display current bypass settings
-            embed = discord.Embed(colour=discord.Colour.blue())
-            embed.add_field(
-                name="Bypass configuration",
-                value=self.config["bypass_channels"],
-            )
-            return await ctx.send(embed=embed)
+        await self.get_log_channel().send(f"{time}: {member.name} {msg}")
 
 async def setup(bot):
     await bot.add_cog(Surveillance(bot))
 
+# TODO: Blacklist with Gsheets
 # TODO: Helping method for stats observing in graphs
