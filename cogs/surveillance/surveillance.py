028a4f51f3123a3b7a56b8ea581b3cd774e2d00b
freezpmark <p.markus1337@gmail.com>
2022-05-19 13:39:09 +0200

    [UPD] surveillance - make conditions more readable

--- cogs/surveillance/surveillance.py
+++ cogs/surveillance/surveillance.py
@@ -27,109 +27,80 @@ class Surveillance(commands.Cog):
 
     @commands.Cog.listener()
     async def on_presence_update(self, before, after):
-        time = self.get_time()
 
-        if (
-            before.status == discord.Status.offline
-        ) and after.status != discord.Status.offline:
-            await self.get_log_channel().send(
-                f"{time}: {after.name} has come online."
-            )
-        if (
-            before.status == discord.Status.idle
-        ) and after.status != discord.Status.idle:
-            await self.get_log_channel().send(
-                f"{time}: {after.name} is no longer idle."
-            )
-        if (
-            before.status != discord.Status.dnd
-        ) and after.status == discord.Status.dnd:
-            await self.get_log_channel().send(
-                f"{time}: {after.name} has set DND status."
-            )
-        if (
-            before.status == discord.Status.dnd
-        ) and after.status != discord.Status.dnd:
-            await self.get_log_channel().send(
-                f"{time}: {after.name} is no longer DND."
-            )
-        if (
-            before.status != discord.Status.idle
-        ) and after.status == discord.Status.idle:
-            await self.get_log_channel().send(
-                f"{time}: {after.name} has gone idle."
-            )
-        elif before.activity != after.activity and after.activity is None:
-            await self.get_log_channel().send(
-                f"{time}: {after.name} has stopped {before.activity.name}."
-            )
-        elif before.activity != after.activity and before.activity is None:
-            await self.get_log_channel().send(
-                f"{time}: {after.name} has started {after.activity.name}."
-            )
-        if (
-            before.status != discord.Status.offline
-        ) and after.status == discord.Status.offline:
-            await self.get_log_channel().send(
-                f"{time}: {after.name} has gone offline."
-            )
+        off     = (before.status != discord.Status.offline) and after.status == discord.Status.offline
+        no_off  = (before.status == discord.Status.offline) and after.status != discord.Status.offline
+        idle    = (before.status != discord.Status.idle) and after.status == discord.Status.idle
+        no_idle = (before.status == discord.Status.idle) and after.status != discord.Status.idle
+        dnd     = (before.status != discord.Status.dnd) and after.status == discord.Status.dnd
+        no_dnd  = (before.status == discord.Status.dnd) and after.status != discord.Status.dnd
+        act     = before.activity != after.activity and before.activity is None
+        no_act  = before.activity != after.activity and after.activity is None
+
+        msgs = []
+        if off:
+            msgs.append("has come online.")
+        if no_idle:
+            msgs.append("is no longer idle.")
+        if dnd:
+            msgs.append("has set DND status.")
+        if no_dnd:
+            msgs.append("is no longer DND.")
+        if idle:
+            msgs.append("has gone idle.")
+        elif no_act:
+            msgs.append(f"has stopped {before.activity.name}.")
+        elif act:
+            msgs.append(f"has started {after.activity.name}.")
+        if no_off:
+            msgs.append("has gone offline.")
+
+        time = self.get_time()
+        for msg in msgs:
+            await self.get_log_channel().send(f"{time}: {after.name} {msg}")
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        time = self.get_time()
 
-        if ((before.deaf is False) and after.deaf) or (
-            (before.self_deaf is False) and after.self_deaf
-        ):
-            await self.get_log_channel().send(
-                f"{time}: {member.name} has been deafened."
-            )
-        elif ((before.deaf is True) and not after.deaf) or (
-            (before.self_deaf is True) and not after.self_deaf
-        ):
-            await self.get_log_channel().send(
-                f"{time}: {member.name} has been undeafened."
-            )
-        elif ((before.mute is False) and after.mute) or (
-            (before.self_mute is False) and after.self_mute
-        ):
-            await self.get_log_channel().send(
-                f"{time}: {member.name} has been muted."
-            )
-        elif ((before.mute is True) and not after.mute) or (
-            (before.self_mute is True) and not after.self_mute
-        ):
-            await self.get_log_channel().send(
-                f"{time}: {member.name} has been unmuted."
-            )
-        elif (before.self_video is False) and after.self_video:
-            await self.get_log_channel().send(
-                f"{time}: {member.name} has started broadcasting a video."
-            )
-        elif (before.self_video is True) and not after.self_video:
-            await self.get_log_channel().send(
-                f"{time}: {member.name} has stopped broadcasting a video."
-            )
-        elif (before.afk is False) and after.afk:
-            await self.get_log_channel().send(
-                f"{time}: {member.name} has gone afk."
-            )
-        elif (before.afk is True) and not after.afk:
-            await self.get_log_channel().send(
-                f"{time}: {member.name} is no longer afk."
-            )
-        elif before.channel != after.channel and after.channel is None:
-            await self.get_log_channel().send(
-                f"{time}: {member.name} has left {before.channel} channel."
-            )
-        elif before.channel != after.channel and before.channel is None:
-            await self.get_log_channel().send(
-                f"{time}: {member.name} has joined {after.channel} channel."
-            )
-        elif before.channel != after.channel:
-            await self.get_log_channel().send(
-                f"{time}: {member.name} has moved from {before.channel} channel into {after.channel}."
-            )
+        deaf    = ((before.deaf is False) and after.deaf) or ((before.self_deaf is False) and after.self_deaf)
+        no_deaf = ((before.deaf is True) and not after.deaf) or ((before.self_deaf is True) and not after.self_deaf)
+        mute    = ((before.mute is False) and after.mute) or ((before.self_mute is False) and after.self_mute)
+        no_mute = ((before.mute is True) and not after.mute) or ((before.self_mute is True) and not after.self_mute)
+        bcast   = (before.self_video is False) and after.self_video
+        no_bcast = (before.self_video is True) and not after.self_video
+        afk     = (before.afk is False) and after.afk
+        no_afk  = (before.afk is True) and not after.afk
+        left    = before.channel != after.channel and after.channel is None
+        join    = before.channel != after.channel and before.channel is None
+        move    = before.channel != after.channel
+
+        msgs = []
+        if deaf:
+            msgs.append("has been deafened.")
+        elif no_deaf:
+            msgs.append("has been undeafened.")
+        elif mute:
+            msgs.append("has been muted.")
+        elif no_mute:
+            msgs.append("has been unmuted.")
+        elif bcast:
+            msgs.append("has started broadcasting a video.")
+        elif no_bcast:
+            msgs.append("has stopped broadcasting a video.")
+        elif afk:
+            msgs.append("has gone afk.")
+        elif no_afk:
+            msgs.append("is no longer afk.")
+        elif left:
+            msgs.append(f"has left {before.channel} channel.")
+        elif join:
+            msgs.append(f"has joined {after.channel} channel.")
+        elif move:
+            msgs.append(f"has moved from {before.channel} channel into {after.channel}.")
+
+        time = self.get_time()
+        for msg in msgs:
+            await self.get_log_channel().send(f"{time}: {after.name} {msg}")
 
     # More types of actions:
     # DELETED msg_post (when somebody sends a message),
