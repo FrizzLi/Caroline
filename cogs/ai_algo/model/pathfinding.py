3f59454e6fad15f143bd3eb77f8a7410a1a8e627
Freezpmark <p.markus1337@gmail.com>
2022-06-17 11:49:49 +0900

    put old code

--- cogs/ai_algo/model/pathfinding.py
+++ cogs/ai_algo/model/pathfinding.py
@@ -1,6 +1,6 @@
 import heapq
 import pickle
-from copy import copy
+from copy import deepcopy
 from itertools import combinations, permutations
 from pathlib import Path
 from sys import maxsize
@@ -149,17 +149,16 @@ def dijkstra(
             Access via Map[start][destination].dist
     """
 
-    nodes = copy(nodes_map.nodes)
-    nodes[start].dist = 0
+    nodes_map[start].dist = 0
     heap = []  # type: List[Node]
-    heapq.heappush(heap, nodes[start])
+    heapq.heappush(heap, nodes_map[start])
 
     while heap:
         node = heapq.heappop(heap)
         for move in moves:
             next_pos = node.pos[0] + move[0], node.pos[1] + move[1]
             if passable(next_pos, nodes_map):
-                next_node = nodes[next_pos]
+                next_node = nodes_map[next_pos]
                 step_dist = get_next_dist(node.terr, next_node.terr, climb)
                 next_node_dist = node.dist + step_dist
 
@@ -168,7 +167,7 @@ def dijkstra(
                     next_node.parent = node.pos
                     heapq.heappush(heap, next_node)
 
-    return nodes
+    return nodes_map
 
 
 def a_star(
@@ -193,11 +192,10 @@ def a_star(
             Access via Map[start][destination].dist
     """
 
-    nodes = copy(nodes_map.nodes)
-    nodes[start].g = 0
+    nodes_map[start].g = 0
     open_list = []  # type: List[Node]
     close_list = []
-    heapq.heappush(open_list, nodes[start])
+    heapq.heappush(open_list, nodes_map[start])
 
     while open_list:
         node = heapq.heappop(open_list)
@@ -208,16 +206,16 @@ def a_star(
         for move in moves:
             next_pos = node.pos[0] + move[0], node.pos[1] + move[1]
             if passable(next_pos, nodes_map) and next_pos not in close_list:
-                next_node = nodes[next_pos]
+                next_node = nodes_map[next_pos]
 
                 # heuristic - distance between destination and next_node
                 x_diff = abs(next_node.pos[0] - dest[0])
                 y_diff = abs(next_node.pos[1] - dest[1])
-                h = x_diff + y_diff
+                h = x_diff + y_diff  
                 
                 # distance between start and next_node
                 step_dist = get_next_dist(node.terr, next_node.terr, climb)
-                g = node.g + step_dist
+                g = node.g + step_dist  
 
                 f = g + h  # estimated distance between start and destination
                 if f < next_node.dist:
@@ -228,7 +226,7 @@ def a_star(
                 if next_node not in open_list:
                     heapq.heappush(open_list, next_node)
 
-    return nodes
+    return nodes_map
 
 
 def naive_permutations(
@@ -261,7 +259,7 @@ def naive_permutations(
 
 
 def held_karp(
-    pro_data: Dict[Tuple[int, int], Map], nodess, subset_size: int
+    pro_data: Dict[Tuple[int, int], Map], subset_size: int
 ) -> Tuple[List[Any], int]:
     """Finds the shortest combination of paths between properties
     using Held Karp's algorithm.
@@ -275,8 +273,7 @@ def held_karp(
         Tuple[List[Any], int]: (order of properties' coordinates, distance)
     """
 
-    # points, home, start = tuple(pro_data.values())[0].properties.values()
-    points, home, start = pro_data.properties.values()
+    points, home, start = tuple(pro_data.values())[0].properties.values()
     points = frozenset(points)
 
     key = Tuple[Tuple[int, int], FrozenSet[int]]
@@ -291,14 +288,14 @@ def held_karp(
                 routes = []
                 if comb_set == frozenset():  # case for home starting
                     cost = (
-                        nodess[home][dest].dist + nodess[start][home].dist
+                        pro_data[home][dest].dist + pro_data[start][home].dist
                     )
                     nodes[dest, frozenset()] = cost, home
                 else:
                     for begin in comb_set:  # single val from set
                         sub_comb = comb_set - frozenset({begin})
                         prev_cost = nodes[begin, sub_comb][0]
-                        cost = nodess[begin][dest].dist + prev_cost
+                        cost = pro_data[begin][dest].dist + prev_cost
                         routes.append((cost, begin))
                     nodes[dest, comb_set] = min(routes)
 
@@ -361,26 +358,25 @@ def find_shortest_distances(
             is measured with abs(current terrain number - next terrain number)
 
     Returns:
-        Dict[Tuple[int, int], Node]: Contains distances between all properties.
+        Dict[Tuple[int, int], Map]: Contains distances between all properties.
             Access via Dict[start][destination].dist
     """
 
     points, home, start = nodes_map.properties.values()
 
-    from_start_to_home = a_star(nodes_map, moves, climb, start, home)
-    from_home_to_all = dijkstra(nodes_map, moves, climb, home)
+    from_start_to_home = a_star(deepcopy(nodes_map), moves, climb, start, home)
+    from_home_to_all = dijkstra(deepcopy(nodes_map), moves, climb, home)
     from_points_to_all = {
-        point: dijkstra(nodes_map, moves, climb, point)
+        point: dijkstra(deepcopy(nodes_map), moves, climb, point)
         for point in points
     }
 
-    shortest_distance_nodes_between_properties = {
+    shortest_distances_between_properties = {
         start: from_start_to_home,
         home: from_home_to_all,
         **from_points_to_all,
     }
-
-    return shortest_distance_nodes_between_properties
+    return shortest_distances_between_properties
 
 
 def get_paths(
@@ -541,10 +537,10 @@ def find_shortest_path(
             algorithm,
         )
 
-        nodes = find_shortest_distances(nodes_map, moves, climb)
+        nodes_map = find_shortest_distances(nodes_map, moves, climb)
         # ! START HERE
-        pro_order, dist = alg_opts[algorithm](nodes_map, nodes, visit_points_amount)
-        paths = get_paths(nodes, pro_order)
+        pro_order, dist = alg_opts[algorithm](nodes_map, visit_points_amount)
+        paths = get_paths(nodes_map, pro_order)
 
         print_solution(paths, dist)
 
