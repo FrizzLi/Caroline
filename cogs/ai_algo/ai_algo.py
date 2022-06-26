d3ce0fe5dce2a1c2823163a55980e9c71646f814
Freezpmark <p.markus1337@gmail.com>
2022-06-26 23:10:53 +0900

    [UPD] ai_algo update according to changes in model

--- cogs/ai_algo/ai_algo.py
+++ cogs/ai_algo/ai_algo.py
@@ -1,12 +1,11 @@
-import os
-
-from discord import File
-from discord.ext import commands
+from pathlib import Path
 
 import cogs.ai_algo.model.evolution as evo
 import cogs.ai_algo.model.forward_chain as chain
 import cogs.ai_algo.model.pathfinding as path
 import cogs.ai_algo.view as view
+from discord import File
+from discord.ext import commands
 
 
 class AiAlgo(commands.Cog):
@@ -29,22 +28,22 @@ class AiAlgo(commands.Cog):
     path_algorithm = "HK"
     path_visit_points_amount = None
 
-    chain_save_fname_facts = "facts"
-    chain_load_fname_facts = "facts_init"
-    chain_load_fname_rules = "rules"
+    chain_fname_save_facts = "facts"
+    chain_fname_load_facts = "facts_init"
+    chain_fname_load_rules = "rules"
     chain_step_by_step = True
-    chain_facts_random_order = True
+    chain_randomize_facts_order = True
 
     view_skip_rake = False
 
     async def send_file_message(self, ctx):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        gif_path = f"{current_dir}\\data\\{self.shared_fname}.gif"
+        current_dir = Path(__file__).parents[1]
+        gif_path = Path(f"{current_dir}/data/{self.shared_fname}.gif")
         with open(gif_path, "rb") as file:
             await ctx.message.channel.send(file=File(file))
 
-    @commands.command(brief="Creates gif and shows it. (pars in file)")
-    async def create(self, ctx):
+    @commands.command(brief="Runs AI algs and shows gif animation of it.")
+    async def run_ai(self, ctx):
         evo_parameters = dict(
             begin_from=self.evo_begin_create,
             query=self.evo_query,
@@ -62,18 +61,18 @@ class AiAlgo(commands.Cog):
         )
 
         chain_parameters = dict(
-            save_fname_facts=self.chain_save_fname_facts,
-            load_fname_facts=self.chain_load_fname_facts,
-            load_fname_rules=self.chain_load_fname_rules,
+            save_fname_facts=self.chain_fname_save_facts,
+            load_fname_facts=self.chain_fname_load_facts,
+            load_fname_rules=self.chain_fname_load_rules,
             step_by_step=self.chain_step_by_step,
             facts_amount=self.shared_points_amount + 1,
-            facts_random_order=self.chain_facts_random_order,
+            facts_random_order=self.chain_randomize_facts_order,
             fname=self.shared_fname,
         )
 
         evo.create_maps(**evo_parameters)
-        path.findShortestPath(**path_parameters)
-        chain.runProduction(**chain_parameters)
+        path.find_shortest_path(**path_parameters)
+        chain.run_production(**chain_parameters)
         view.createGif(
             self.shared_fname, self.view_skip_rake, self.shared_climb
         )
@@ -84,11 +83,6 @@ class AiAlgo(commands.Cog):
 async def setup(bot):
     await bot.add_cog(AiAlgo(bot))
 
-# TODO: var names outside and inside functions
-# NOTE: Naming onsistency in f. calls is rly not necessary-using_facts ex.
-# TODO: chain - optimize creating props
-
-# TODO: use consistent variable naming inside and outside functions?
 # TODO: gspread parameter loading
 # TODO: update ai repo and make local parameter loading there
 # TODO: clean commits (?)
