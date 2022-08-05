0dc4295c1a6c6ff6ab3c4fcecd9156cbd3dfd336
Freezpmark <p.markus1337@gmail.com>
2022-08-05 11:22:48 +0900

    [IMP] move simple_commands module to surveillance (too small)

--- cogs/surveillance/surveillance.py
+++ cogs/surveillance/surveillance.py
@@ -1,3 +1,4 @@
+import random
 from datetime import datetime
 
 import discord
@@ -104,6 +105,41 @@ class Surveillance(commands.Cog):
 
         time = self.get_time()
         await self.get_log_channel().send(f"{time}: {member.name} {msg}")
+
+    # Simple commands
+    @commands.command(brief="Rolls a number between 1 and 100. (roll 1000)")
+    async def roll(self, ctx, max_=100):
+        rolled_number = f"{str(random.randint(1, max_))} (1 - {str(max_)})"
+        await ctx.send(f"{ctx.message.author.mention} rolls {rolled_number}")
+
+    @commands.command(brief="Enables Python interactive shell.")
+    async def python(self, ctx):
+        await ctx.send(f'Python mode activated! Exit by "{ctx.prefix}"')
+        await self.bot.change_presence(activity=discord.Game(name="Python"))
+
+        def check(message):
+            return message.channel == ctx.channel
+
+        msg = await self.bot.wait_for("message", check=check)
+        ans = 0
+
+        while not msg.content.startswith(f"{ctx.prefix}"):
+            try:  # with return
+                ans = eval(msg.content)  # pylint: disable=eval-used
+                await ctx.send(ans)
+            except Exception:  # no return
+                try:
+                    exec(msg.content)  # pylint: disable=exec-used
+                except Exception as err:  # invalid input
+                    await ctx.send(err)
+            msg = await self.bot.wait_for("message", check=check)
+
+        await self.bot.change_presence(
+            activity=discord.Activity(
+                type=discord.ActivityType.listening, name=f"{ctx.prefix}help"
+            )
+        )
+        await ctx.send("Python mode deactivated!")
 
 
 async def setup(bot):
