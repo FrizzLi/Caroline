250c0eef4e313463332867ba3982f4214fb155d8
Freezpmark <p.markus1337@gmail.com>
2022-09-24 15:11:28 +0900

    [IMP] save detector's results into file

--- cogs/korean/vocabulary_detector.py
+++ cogs/korean/vocabulary_detector.py
@@ -1,30 +1,26 @@
-
 from pathlib import Path
 
 from konlpy.tag import Okt
 
-vocabulary = []
 okt = Okt()
 
 source_dir = Path(__file__).parents[0]
-fre_json = Path(f"{source_dir}/data/text_to_vocab.txt")
+text_file_name = Path(f"{source_dir}/data/text_to_vocab.txt")
+vocab_file_name = Path(f"{source_dir}/data/reading_vocab.txt")
 
-with open(fre_json, encoding="utf-8") as f:
-    korean_text = f.read()
+with open(text_file_name, encoding="utf-8") as file:
+    korean_text = file.read()
     print('Tokenizing Korean text...', end='')
     tokenized_text = okt.pos(korean_text, norm=True, stem=True)
     print('Done')
 
-for token in tokenized_text:
-    try:
-        if (token[1] == 'Punctuation' or token[1] == 'Foreign'):
-            print(f'Skipping garbage... ({token}')
-            continue
-    except Exception as e:
-        print('Error skipping token:', e)
+with open(vocab_file_name, "w", encoding="utf-8") as file:
+    for token in tokenized_text:
+        try:
+            if (token[1] == 'Punctuation' or token[1] == 'Foreign'):
+                print(f'Skipping garbage... ({token}')
+                continue
+        except Exception as e:
+            print('Error skipping token:', e)
 
-    vocabulary.append({
-        'Word': token[0],
-        'Type': token[1],
-    })
-    print(token[0], token[1])
+        file.write(f"{token[0]} {token[1]}\n")
