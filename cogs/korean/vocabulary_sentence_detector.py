43d2b218bd139da318e9585e06066a89e43a046a
Freezpmark <p.markus1337@gmail.com>
2022-10-13 16:43:52 +0900

    [UPD] rename vocabularization data

--- cogs/korean/vocabulary_sentence_detector.py
+++ cogs/korean/vocabulary_sentence_detector.py
@@ -5,8 +5,8 @@ from konlpy.tag import Okt
 okt = Okt()
 
 source_dir = Path(__file__).parents[0]
-text_file_name = Path(f"{source_dir}/data/text_to_vocab.txt")
-vocab_file_name = Path(f"{source_dir}/data/reading_vocab.txt")
+text_file_name = Path(f"{source_dir}/data/text_to_be_vocabularized.txt")
+vocab_file_name = Path(f"{source_dir}/data/text_vocabularized.txt")
 
 with open(text_file_name, encoding="utf-8") as file:
     korean_text = file.read()
