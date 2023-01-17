6c29ec896003bc15b03daa7617257cb8f4e28b13
Freezpmark <p.markus1337@gmail.com>
2023-01-17 09:31:17 +0900

    [FIX] song skipping (workaround)

--- cogs/music/player.py
+++ cogs/music/player.py
@@ -1,5 +1,6 @@
 import asyncio
 import random
+import time
 
 from discord.errors import ClientException
 
@@ -26,7 +27,8 @@ class MusicPlayer:
         "loop_queue",
         "loop_track",
         "timestamp",
-        "view"
+        "view",
+        "workaround",
     )
 
     def __init__(self, interaction, music):
@@ -37,7 +39,7 @@ class MusicPlayer:
         self.next = asyncio.Event()
 
         self.np_msg = None
-        self.volume = 0.05
+        self.volume = 0.1
         self.current_pointer = 0
         self.next_pointer = -1
         self.loop_queue = False
@@ -46,6 +48,7 @@ class MusicPlayer:
         self.timestamp = 0
 
         self.view = None
+        self.workaround = 1
 
         interaction.client.loop.create_task(self.player_loop())
 
@@ -55,17 +58,18 @@ class MusicPlayer:
         await self.interaction.client.wait_until_ready()
 
         while not self.interaction.client.is_closed():
+            print("1 INTO CLEAR")
             self.next.clear()
-            print("1 CLEAR")
-            # Log info: Normal flow is going from 1 to 10
-            #   (3 if qloop triggered, or no next song; 5 if closing
+            print("2 FROM CLEAR")
+            # Log info: Normal flow is going from 1 to 9
+            #   (4 if qloop triggered, or no next song
             # What is regather for? How is it different from create source
             # Cannot we use regather for all so it would be faster?
 
             try:
                 if not self.loop_track:
                     self.next_pointer += 1  # next song
-                    print("2 POINTER++")
+                    print("3 POINTER++")
                 if self.next_pointer >= len(self.queue):
                     if self.loop_queue:
                         self.next_pointer = 0  # queue loop
@@ -74,52 +78,81 @@ class MusicPlayer:
                         print("3 INTO WAIT")
                         await self.next.wait()
                         print("3 FROM WAIT")
+                        print("3 INTO CLEAR")
                         self.next.clear()
-                        print("3 CLEAR")
+                        print("3 FROM CLEAR")
 
-                print("4 INTO CURRENT_POINTER UPDATE")
+                # self.old_pointer = self.current_pointer
                 self.current_pointer = self.next_pointer
                 source = self.queue[self.current_pointer]
 
-            except asyncio.TimeoutError:
-                print("5 DESTROY")
+            except (IndexError, asyncio.TimeoutError):
                 return self.destroy(self.interaction.guild)
 
-            if not isinstance(source, YTDLSource):
-                # Source was probably a stream (not downloaded)
-                # So we should regather to prevent stream expiration
-
-                try:
-                    print("6 GOING TO REGATHER")
-                    source = await YTDLSource.regather_stream(
-                        source,
-                        loop=self.interaction.client.loop,
-                        timestamp=self.timestamp,
-                    )
-                except Exception as err:
-                    await self.interaction.channel.send(
-                        f"Error:\n```css\n[{err}]\n```"
-                    )
-                    continue
-                self.timestamp = 0
-                print("7 TIMESTAMP 0")
-
-            source.volume = self.volume
+            # self.regather_count = 0
             try:
-                print("8 CALL LOOP THREADSAFE")
-                call_next = self.interaction.client.loop.call_soon_threadsafe
-                self.interaction.guild.voice_client.play(
-                    source,
-                    after=lambda _: call_next(self.next.set),
-                )
+                while self.workaround:
+                    # REGATHERING
+                    if not isinstance(source, YTDLSource):
+                        # Source was probably a stream (not downloaded)
+                        # So we should regather to prevent stream expiration
+
+                        print("4 INTO REGATHER")
+                        re_source = await YTDLSource.regather_stream(
+                            source,
+                            loop=self.interaction.client.loop,
+                            timestamp=self.timestamp,
+                        )
+                        # self.regather_count += 1
+                        self.timestamp = 0  # Why
+                    re_source.volume = self.volume
+
+                    # PLAYING, WORKAROUND
+                    print("5 INTO PLAY")  # proste tu preskoci a ide dalej
+                    self.workaround = 0
+                    self.interaction.guild.voice_client.play(re_source,
+                        after=self.play_next_song
+                    )
+                    print(f"6 FROM PLAY {self.workaround}")
+                    print("Sleep abit!")
+                    time.sleep(1)
+
+                    # call_next = self.interaction.client.loop.call_soon_threadsafe
+                    # self.interaction.guild.voice_client.play(
+                    #     source,
+                    #     after=lambda _: call_next(self.next.set),
+                    # )
+                    # print(f"{source.duration}\n{source.requester}\n{source.title}\n{source.view_count}\n{source.volume}\n{source.webpage_url}\n{source._is_protocol}\n{source._volume}")
+
             except (ClientException, AttributeError) as err:
-                print(err)
+                print(f"ClientException: {err}")
+                return
+            except AttributeError as err:
+                print(f"AttributeError: {err}")
+                return
+            except Exception as err:
+                print(f"Exception: {err}")
+                await self.interaction.channel.send(
+                    f"Error:\n```css\n[{err}]\n```"
+                )
                 return
 
-            self.view = PlayerView(self, source)
-            print("9 INTO UPDATE MSG")
+            self.next.clear()
+            print(f"7 FROM WHILE PLAY {self.workaround}")
+            self.view = PlayerView(self, re_source)
             await self.update_player_status_message()
-            print("10 MSG UPDATED")
+
+            print("8 INTO WAIT")
+            await self.next.wait()
+            print("10 FROM WAIT")
+
+    def play_next_song(self, error=None):
+        if error:
+            print(f"halo? {error}")
+        self.workaround = 1
+
+        print("9 SET")
+        self.next.set()
 
     async def update_player_status_message(self):
         if not self.np_msg:  # there is no np msg anywhere
@@ -135,8 +168,6 @@ class MusicPlayer:
             self.np_msg = await self.interaction.channel.send(
                 content=self.view.msg, view=self.view
             )
-
-        await self.next.wait()
 
 
     def shuffle(self):
