52fb4fbb428dd76a225bcbbf4d2596944a2f869c
Freezpmark <p.markus1337@gmail.com>
2023-02-07 12:59:08 +0900

    [UPD] main TODOs - finish ai docs during test implementation

--- main.py
+++ main.py
@@ -68,4 +68,4 @@ bot.run(TOKEN)
 # TODO: Polish Discord related stuff: Pylint, Document, Optimize code and Async
 # TODO: Logs
 # TODO: Resolve all weird pylint errors
-# TODO: Tests (setup.py, pytest from reqs, _apply_actions test remove)
+# TODO: Tests (setup.py, pytest from reqs, AI: _apply_actions test (rmv), docs)
