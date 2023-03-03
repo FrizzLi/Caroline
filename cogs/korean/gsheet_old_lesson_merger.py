2e777127e1f49ccfd7d17e973a67c8ab4dbfdd6b
Freezpmark <p.markus1337@gmail.com>
2023-03-03 13:17:24 +0900

    [UPD] polish and add docs for gsheet_column_merger

--- cogs/korean/gsheet_old_lesson_merger.py
+++ cogs/korean/gsheet_old_lesson_merger.py
@@ -9,6 +9,15 @@ import pandas as pd
 
 
 def create_df(worksheet):
+    """Creates a dataframe out from a google worksheet.
+
+    Args:
+        worksheet (gspread.worksheet.Worksheet): worksheet table
+
+    Returns:
+        pandas.core.frame.DataFrame: dataframe table
+    """
+
     df = pd.DataFrame(worksheet.get_all_records())
 
     for row in df.itertuples():
@@ -27,23 +36,40 @@ def create_df(worksheet):
     return df
 
 
-def get_worksheet(ws_name):
+def get_worksheet(gs_name, ws_name):
+    """Gets a worksheet instance that is pulled from google sheets.
+
+    Args:
+        gs_name (str): name of the google sheets
+        ws_name (str): name of the worksheet (a tab of google sheets)
+
+    Returns:
+        gspread.worksheet.Worksheet: worksheet table
+    """
+
     credentials_dict_str = os.environ["GOOGLE_CREDENTIALS"]
     credentials_dict = json.loads(credentials_dict_str)
     g_credentials = gspread.service_account_from_dict(credentials_dict)
-    g_sheet = g_credentials.open("Korea - Vocabulary")
+    g_sheet = g_credentials.open(gs_name)
     worksheet = g_sheet.worksheet(ws_name)
 
     return worksheet
 
 
 def update_worksheet(dataframe, worksheet):
+    """Updates the google worksheet with the dataframe.
+
+    Args:
+        dataframe (pandas.core.frame.DataFrame): dataframe table
+        worksheet (gspread.worksheet.Worksheet): worksheet table
+    """
+
     dataframe = dataframe.fillna("")
     df_list = [dataframe.columns.values.tolist()]
     df_list += dataframe.values.tolist()
     worksheet.update(df_list, value_input_option="USER_ENTERED")
 
 
-vocab_g_ws = get_worksheet("Level 1-2 (backup)")
-vocab_df = create_df(vocab_g_ws)
-update_worksheet(vocab_df, vocab_g_ws)
+google_worksheet = get_worksheet("Korea - Vocabulary", "Level 1-2 (modified)")
+google_dataframe = create_df(google_worksheet)
+update_worksheet(google_dataframe, google_worksheet)
