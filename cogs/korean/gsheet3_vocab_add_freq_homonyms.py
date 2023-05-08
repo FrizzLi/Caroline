dec5dd7be91f6e24ae4dbee9203c6096c8a4b350
Freezpmark <p.markus1337@gmail.com>
2023-05-09 08:19:50 +0900

    [UPD] remake gsheet3 (functions, polish, docs..)

--- cogs/korean/gsheet3_vocab_add_freq_homonyms.py
+++ cogs/korean/gsheet3_vocab_add_freq_homonyms.py
@@ -1,88 +1,109 @@
 """
-"Korea - Vocabulary" Google Spreadsheet:
- - adds numbered freq words (more meanings) below base word in "Level 1-2" tab
+"Korea - Vocabulary" Spreadsheet:
+ - adds numbered freq words (multiple meanings) below base word
+   in "Level 1-2 (raw)" tab
+ - NOTE that it creates duplicates inside the worksheet
+ - NOTE that the outcome of this script hasn't been used in the end
 """
 
 import json
-import os
+import sys
 from pathlib import Path
 
-import gspread
 import pandas as pd
 
-# Function to insert row in the dataframe
-def Insert_row_(row_number, df, row_value):
-    # Slice the upper half of the dataframe
-    df1 = df[0:row_number]
 
-    # Store the result of lower half of the dataframe
-    df2 = df[row_number:]
+def _df_row_insert(row_num, old_df, row_val):
+    """Inserts a row into dataframe.
 
-    # Insert the row in the upper half dataframe
-    df1.loc[row_number]=row_value
+    Args:
+        row_num (int): row number
+        old_df (pandas.core.frame.DataFrame): dataframe into which we insert
+        row_val (list): row values
 
-    # Concat the two dataframes
-    df_result = pd.concat([df1, df2])
+    Returns:
+        pandas.core.frame.DataFrame: new dataframe with inserted row_val
+    """
 
-    # Reassign the index labels
-    df_result.index = [*range(df_result.shape[0])]
+    df1 = old_df[0:row_num]
+    df2 = old_df[row_num:]
+    df1.loc[row_num] = row_val
+    new_df = pd.concat([df1, df2])
+    new_df.index = [*range(new_df.shape[0])]
 
-    # Return the updated dataframe
-    return df_result
+    return new_df
 
-credentials_dict_str = os.environ["GOOGLE_CREDENTIALS"]
-credentials_dict = json.loads(credentials_dict_str)
-g_credentials = gspread.service_account_from_dict(credentials_dict)
-g_sheet_main = g_credentials.open("Korea - Vocabulary")
 
-src_dir = Path(__file__).parents[0]
-fre_json = Path(f"{src_dir}/data/vocab_sources/freq_dict_kor.json")
+def _fill_lvl_df(freq, lvl_df):
+    """Fills up lvl dataframe with homonyms below base word.
 
-with open(fre_json, encoding="utf-8") as fre:
-    freq = json.load(fre)
+    Args:
+        freq (Dict[str, Dict[str, str]]): freq source
+        lvl_df (pandas.core.frame.DataFrame): dataframe to be filled
 
-lvl_1_2_g_work_sheet = g_sheet_main.worksheet("Level 1-2 (modified)")
-lvl_1_2_df = pd.DataFrame(lvl_1_2_g_work_sheet.get_all_records())
+    Returns:
+        pandas.core.frame.DataFrame: filled dataframe
+    """
 
-added_num = 0
+    added_num = 0
+    for row in lvl_df.itertuples():
+        for num in range(1, 6):
+            if not row.Korean:
+                continue
+            if row.Korean[-1].isdigit():
+                continue
+            else:
+                row_korean = row.Korean
+            numered_word = f"{row_korean}{num}"
+            if numered_word in freq:
+                added_num += 1
+                word = freq.pop(numered_word)
+                row_value = [
+                    "",
+                    numered_word,
+                    "",
+                    word["content"],
+                    "",
+                    word["example_en"],
+                    word["example_kr"],
+                    word["rank"],
+                    word["type"],
+                    word["romanization"],
+                    word["frequency"],
+                    word["disp"],
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+                index = row.Index + added_num
+                lvl_df = _df_row_insert(index, lvl_df, row_value)
 
-for row in lvl_1_2_df.itertuples():
-    for num in range(1, 6):
-        if not row.Korean:
-            continue
-        if row.Korean[-1].isdigit():
-            continue
-        else:
-            row_korean = row.Korean
-        numered_word = f"{row_korean}{num}"
-        if numered_word in freq:
-            added_num += 1
-            word = freq.pop(numered_word)
-            row_value = [
-                "",
-                numered_word,
-                "",
-                word["freq_eng"],
-                "",
-                word["example_en"],
-                word["example_kr"],
-                word["rank"],
-                word["type"],
-                word["romanization"],
-                word["frequency"],
-                word["disp"],
-                "",
-                "",
-                "",
-                ""
-            ]
-            index = row.Index + added_num
-            lvl_1_2_df = Insert_row_(index, lvl_1_2_df, row_value)
+    return lvl_df
 
-df = lvl_1_2_df.sort_index().reset_index(drop=True)
 
-# save to gsheets
-lvl_1_2_df = lvl_1_2_df.fillna("")
-lvl_1_2_list = [lvl_1_2_df.columns.values.tolist()]
-lvl_1_2_list += lvl_1_2_df.values.tolist()
-lvl_1_2_g_work_sheet.update(lvl_1_2_list, value_input_option="USER_ENTERED")
+def add_homonyms_below_base_word():
+    """Adds homonyms below base word in Level 1-2 (raw) worksheet."""
+
+    src_dir = Path(__file__).parents[0]
+    fre_json = Path(f"{src_dir}/data/vocab_sources/freq_dict_kor.json")
+    with open(fre_json, encoding="utf-8") as fre:
+        freq = json.load(fre)
+
+    worksheets, worksheets_df = utils.get_worksheets(
+        "Korea - Vocabulary", ("Level 1-2 (raw)",)
+    )
+    lvl_worksheet, lvl_df = worksheets[0], worksheets_df[0]
+
+    lvl_df = _fill_lvl_df(freq, lvl_df)
+
+    utils.update_worksheet(lvl_worksheet, lvl_df)
+
+
+if __name__ == "__main__":
+    sys.path.append(str(Path(__file__).parents[2]))
+    import utils
+
+    add_homonyms_below_base_word()
+
+# TODO: rename to gsheet_vocab...
