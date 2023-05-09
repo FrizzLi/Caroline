e21d8641cb5b9957f20313ae7c43d44804cbbe0a
Freezpmark <p.markus1337@gmail.com>
2023-05-10 08:22:24 +0900

    [IMP] gsheet error handling

--- utils.py
+++ utils.py
@@ -7,12 +7,16 @@ import gspread
 import pandas as pd
 
 
+class WorksheetNameNotInList(Exception):
+    """Exception for Worksheet variable type: only lists are allowed."""
+
+
 def get_worksheets(gs_name, ws_names):
     """Gets worksheets of spreadsheet.
 
     Args:
         gs_name (str): name of the spreadsheet
-        ws_names (List[str]): name of the worksheets (a tab of spreadsheet)
+        ws_names (Tuple[str]): name of the worksheets (a tab of spreadsheet)
 
     Returns:
         List[gspread.worksheet.Worksheet, pandas.core.frame.DataFrame]: (
@@ -23,29 +27,37 @@ def get_worksheets(gs_name, ws_names):
 
     credentials_dict_str = os.environ["GOOGLE_CREDENTIALS"]
     credentials_dict = json.loads(credentials_dict_str)
-    g_credentials = gspread.service_account_from_dict(credentials_dict)
-    g_sheet = g_credentials.open(gs_name)
+    google_credentials = gspread.service_account_from_dict(credentials_dict)
+    spreadsheet = google_credentials.open(gs_name)
+    # TODO cr: g_sheet.add_worksheet("Commands Log", df.shape[0], df.shape[1])
 
     worksheets = []
     worksheet_dfs = []
-    for ws_name in ws_names:
-        worksheet = g_sheet.worksheet(ws_name)
-        worksheets.append(worksheet)
-        worksheet_df = pd.DataFrame(worksheet.get_all_records())
-        worksheet_dfs.append(worksheet_df)
+    try:
+        for ws_name in ws_names:
+            worksheet = spreadsheet.worksheet(ws_name)
+            worksheets.append(worksheet)
+            worksheet_df = pd.DataFrame(worksheet.get_all_records())
+            worksheet_dfs.append(worksheet_df)
+    except gspread.exceptions.WorksheetNotFound as err:
+        if len(ws_name) == 1 and ws_names.startswith(ws_name):
+            message = "You probably haven't put your worksheets into the list!"
+            raise WorksheetNameNotInList(message) from err
+        else:
+            raise err
 
     return worksheets, worksheet_dfs
 
 
-def update_worksheet(worksheet, worksheet_df):
+def update_worksheet(ws, ws_df):
     """Updates worksheet with dataframe.
 
     Args:
-        worksheet_df (pandas.core.frame.DataFrame): worksheet dataframe table
-        worksheet (gspread.worksheet.Worksheet): worksheet table
+        ws_df (pandas.core.frame.DataFrame): worksheet dataframe table
+        ws (gspread.worksheet.Worksheet): worksheet table
     """
 
-    worksheet_df = worksheet_df.fillna("")
-    df_list = [worksheet_df.columns.values.tolist()]
-    df_list += worksheet_df.values.tolist()
-    worksheet.update(df_list, value_input_option="USER_ENTERED")
+    ws_df = ws_df.fillna("")
+    df_list = [ws_df.columns.values.tolist()]
+    df_list += ws_df.values.tolist()
+    ws.update(df_list, value_input_option="USER_ENTERED")
